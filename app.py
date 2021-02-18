a8ecda99164caa66bc267cb66426b755252e7afd
rian bogle <rian.bogle@blackboard.com>
2021-02-18 11:49:32 -0700

    flex stack name

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ from dotenv import dotenv_values
 from infra.backstage import BackstageStack
 
 props = dotenv_values()
+stack_name = props.get('TAG_STACK_NAME', 'backstage')
 
 # Using a hosted dns zone requires specifying account and region 
 env =core.Environment(account=props.get('AWS_ACCOUNT'), region=props.get('AWS_REGION', 'us-east-1'))
 
 app = core.App()
-backstage = BackstageStack(app, "backstage", props, env=env)
+backstage = BackstageStack(app, stack_name, props, env=env)
 
 # be nice and tag all these resources so their are attributable
-core.Tags.of(backstage).add("Stack:Name",props.get('TAG_STACK_NAME', 'Backstage'))
+core.Tags.of(backstage).add("Stack:Name",stack_name)
 core.Tags.of(backstage).add("Stack:Author",props.get('TAG_STACK_AUTHOR', 'foo.bar@example.com'))
 
 app.synth()
